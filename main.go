08aa2d4c4df388100dd5a4abdc2de44267038f3c
shale0315 <jacobkubena@pm.me>
2025-06-04 12:18:52 -0500

    Ch2,L2: Added reset/metrics endpoints; separated some handlers

--- main.go
+++ main.go
@@ -1,19 +1,41 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+	hits := cfg.fileserverHits.Load()
+	fmt.Fprintf(w, "Hits: %v", hits)
+}
 
 func main() {
 	const port = "8080"
 
+	apiCfg := apiConfig{}
 	mux := http.NewServeMux()
 	srv := &http.Server{
-		Addr:    ":8080",
+		Addr:    ":" + port,
 		Handler: mux,
 	}
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir("./app")))))
+	mux.HandleFunc("/healthz", readinessEndpoint)
+	mux.Handle("/metrics", http.HandlerFunc(apiCfg.metricsHandler))
+	mux.Handle("/reset", http.HandlerFunc(apiCfg.resetHandler))
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(srv.ListenAndServe())
 }
---
+++ resethandler.go
@@ -0,0 +1,8 @@
+package main
+
+import "net/http"
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.WriteHeader(http.StatusOK)
+}
